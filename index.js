fc9a09b0a776a83165a612b9d2b4a509463348ea
Mateusz <mkibilko@site.pl>
2019-02-15 14:30:19 +0100

    add profile

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const BitgoJS = require('bitgo');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
-const bitgo = new BitgoJS.BitGo();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
@@ -23,10 +22,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('*', (req,res,next) => {
-    let obj = {};
-    obj.token = req.session.user;
-    // let accesToken = req.session.user || null;
-    console.log(obj.token.id);
+    res.locals.user = req.session.user;
     next();
 });
 
@@ -35,8 +31,9 @@ app.get('/', (req,res) => {
 });
 
 app.get('/test', (req,res) => {
-  req.session.test = 'test';
-  res.send("HELLO");
+  bitgo.me({}, (err, user) => {
+    console.log(user);
+  })
 });
 
 app.use('/user', userRouter);
--- routes/user/user.js
+++ routes/user/user.js
@@ -4,23 +4,50 @@ const BitgoJS = require('bitgo');
 
 const bitgo = new BitgoJS.BitGo();
 
-router.get('/profile', (req,res) => {
-    if(typeof req.session.user === 'undefined' ) {
-      bitgo.authenticate({ env: 'test', username: 'mkibilko@site.pl', password: 'heszkewmeszke', otp: '0000000' })
-      .then(function (user) {
-        req.session.user = user.user;
-        res.json(user.user);
-      });
-    }
+router.get('/login', (req, res) => {
+  res.render('user/login');
 });
 
-router.get('/session', (req, res) => {
-    bitgo.me({}, (err, session) => {
-        if (err) {
-            console.log("Error : " + err);
-        }
-        console.log("Session: " + session);
-    })
+router.post('/authorization', (req, res) => {
+
+  if (typeof req.session.user === 'undefined') {
+    bitgo.authenticate({ env: 'test', username: req.body.email, password: req.body.password, otp: '0000000' })
+      .then(function (user) {
+        req.session.user = user.user;
+        res.redirect('/');
+      });
+  }
+
+});
+
+router.post('/logout', (req, res) => {
+  bitgo.logout({})
+    .then(() => {
+      delete req.session.user;
+      res.redirect('/');
+    }).catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get('/profile', (req, res) => {
+  bitgo.me({}, (err, user) => {
+    console.log(user);
+    if (err) {
+      res.locals.error = err;
+    }
+    res.render('user/profile', {
+      user: user
+    });
+  })
+});
+
+router.get('/wallet', (req, res) => {
+  bitgo.wallets().list()
+    .then(function (wallets) {
+      // print the wallets
+      console.dir(wallets);
+    });
 });
 
 module.exports = router;
