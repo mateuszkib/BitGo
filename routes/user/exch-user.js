90b342ad6c84f49a2524c7171f5661fb8cd20c29
Mateusz <mkibilko@site.pl>
2019-03-05 14:30:38 +0100

    create receive address for user

--- routes/user/exch-user.js
+++ routes/user/exch-user.js
@@ -40,23 +40,23 @@ router.get('/profile/:id', (req, res) => {
         for (let i = 0; i < walletsLength; i++) {
             labelWallets.push(new Promise((resolve, reject) => {
                 bitgo.coin(user.idWallet[i].coin).wallets().get({ id: user.idWallet[i].id })
-                .then((wallet) => {
-                    resolve({wallet: wallet, user: user});
-                });
+                    .then((wallet) => {
+                        resolve({ wallet: wallet, user: user });
+                    });
             }))
         }
         let result = Promise.all(labelWallets);
         result.then((data) => {
-            let labels = [];
+            let wallet = [];
             let user = data[0].user;
 
             data.forEach((item) => {
-                labels.push(item.wallet._wallet.label);
+                wallet.push({label: item.wallet._wallet.label, id: item.wallet._wallet.id, coin: item.wallet._wallet.coin});
             });
-
+            console.log(wallet);
             res.render('exch-user/profile', {
-                labels: labels,
-                user: user    
+                labels: wallet,
+                user: user
             });
         })
     });
@@ -71,13 +71,90 @@ router.post('/createWallet/:id', (req, res) => {
             let label = "EX:" + user._id + coin;
             let wallet = await addWallet(coin, label, password);
             let updateUser = await updateUserWalletId(user._id, wallet.wallet._wallet.id, coin);
-            //let redirect = await refreshPage(res, user._id);
         } catch (err) {
             console.log(err);
         }
     }
     createWallet();
+    setTimeout(() => {
+        res.redirect('/exchange/user/profile/' + req.params.id);  
+    }, 4000);
 });
+
+router.get('/profile/:id/:coin/:idWallet/receive', (req,res) => {
+    let coin = req.params.coin;
+    let idWallet = req.params.idWallet;
+    let userId = req.params.id;
+
+    bitgo.coin(coin).wallets().get({id: idWallet})
+    .then((wallet) => {
+        wallet.addresses()
+        .then((addresses) => {
+            console.log(wallet);
+            let totalAddress = addresses.totalAddressCount;
+            let currentAddress = addresses.addresses[totalAddress - 1].address;
+            let currentLabelAddress = addresses.addresses[totalAddress - 1].label;
+            res.render('exch-user/wallet-receive', {
+                addresses: addresses,
+                idWallet: idWallet,
+                coin: coin,
+                current: currentAddress,
+                label: currentLabelAddress,
+                userId: userId
+            })
+        });
+    });
+});
+
+router.get('/profile/:id/:coin/:idWallet', (req,res) => {
+    let coin = req.params.coin;
+    let idWallet = req.params.idWallet;
+    bitgo.coin(coin).wallets().get({id: idWallet})
+    .then((wallet) => {
+        wallet.addresses()
+        .then((addresses) => {
+            console.log(addresses);
+            let totalAddress = addresses.totalAddressCount;
+            let currentAddress = addresses.addresses[totalAddress - 1].address;
+            
+            res.render('exch-user/wallet', {
+                addresses: addresses,
+                idWallet: idWallet,
+                coin: coin,
+                current: currentAddress,
+                
+            })
+        });
+    });
+});
+
+router.post('/createAddress/:idUser/:coin/:idWallet', (req,res) => {
+    let label = req.body.label;
+    let coin = req.params.coin;
+    let walletId = req.params.idWallet;
+    let userId = req.params.idUser;
+
+    async function createAddressForUser() {
+        let result = await createAddress(coin,walletId,label);
+    }
+
+    createAddressForUser();
+    setTimeout(() => {
+        res.redirect('/exchange/user/profile/' + userId + "/" + coin + "/" + walletId + "/receive");
+    }, 3000)
+});
+
+
+function createAddress(coin,id,label) {
+    bitgo.coin(coin).wallets().get({id : id})
+    .then((wallet) => {
+        wallet.createAddress({label: label})
+        .then((address) => {
+            console.log(address);
+        })
+    })
+}
+
 
 function getUser(id) {
     return new Promise((resolve, reject) => {
@@ -85,10 +162,6 @@ function getUser(id) {
             resolve(user);
         })
     });
-}
-
-function refreshPage(res,id){
-    res.redirect('/exchange/user/profile/' + id);
 }
 
 function addWallet(coin, label, password) {
